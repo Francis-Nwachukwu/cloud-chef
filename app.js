85a8c8ca46827671ff8061c0483c69da612dd7fc
Nwachukwu Francis <francisnwachukwu100@gmail.com>
2021-08-17 16:46:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const overlay = document.querySelector('.overlay');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const toggleMobileNav = () => {
+    const mobileNav = document.querySelector('.mobile-nav');
+    if (mobileNav.classList.contains('hide')) {
+        overlay.classList.remove('hide');
+        mobileMenu.classList.add('open');
+    } else {
+        overlay.classList.add('hide');
+        mobileMenu.classList.remove('open');
+    }
+    mobileNav.classList.toggle('hide');
+};
+overlay.addEventListener('click', () => {
+    const mobileNav = document.querySelector('.mobile-nav');
+    if (!mobileNav.classList.contains('hide')) {
+        toggleMobileNav();
+    }
+});
+mobileMenu.addEventListener('click', toggleMobileNav);
